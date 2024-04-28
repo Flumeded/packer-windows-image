d07f5ec5dd77dc1dc30bdd9f3d9a0cdbc8dab03c
flumeded <nikita@dymko.com>
2024-04-28 23:08:18 +0400

    YOLO Push

--- setup/InitialSetup.ps1
+++ setup/InitialSetup.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
 
-# Switch network connection to private mode
-# Required for WinRM firewall rules
+#Switch network connection to private mode
+#Required for WinRM firewall rules
 $profile = Get-NetConnectionProfile
 Set-NetConnectionProfile -Name $profile.Name -NetworkCategory Private
 
@@ -15,3 +15,17 @@ Get-WindowsUpdate -MicrosoftUpdate -install -IgnoreUserInput -acceptall -AutoReb
 winrm quickconfig -quiet
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
+
+
+#PowerShell script to map a network drive with credentials
+#This block will be uncommented and pupulatedby ansible, 
+#it is not really designed to be used as is
+
+# $NetworkPath = "{{ share_path }}"  # Specify the network path
+# $DriveLetter = "Z:"             # Specify the drive letter to assign to the network share
+# $Username = "{{ share_user }}"   # Specify the username
+# $Password = "{{ share_password }}"          # Specify the password
+# $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, ($Password | ConvertTo-SecureString -AsPlainText -Force)
+
+#Map the network drive
+# New-PSDrive -Name ($DriveLetter -replace ':', '') -PSProvider FileSystem -Root $NetworkPath -Credential $Credential -Persist
