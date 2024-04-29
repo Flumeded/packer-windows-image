23234bd0fba7916110dd069ea4600548181a7db4
flumeded <nikita@dymko.com>
2024-04-29 23:22:18 +0400

    Fix regexp escaping

--- setup/win_configuration.ps1
+++ setup/win_configuration.ps1
@@ -3,10 +3,9 @@
 #it is not really designed to be used as is
 
 #Store the credentials securely
-# Invoke-Expression "cmdkey /add:{{ share_path | regex_replace('\\\\(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}).*', '\\1') }} /user:{{ share_user }} /pass:{{ share_password }}"
+# Invoke-Expression "cmdkey /add:{{ share_path | regex_replace('\\\\\\\\(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\\\\.*', '\\1') }} /user:{{ share_user }} /pass:{{ share_password }}"
 
 #Map the network drive
 # Invoke-Expression "net use Z: {{ share_path }} /persistent:yes"
-
-#Install Chocolatey
+#Install Cholote
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
